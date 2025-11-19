da9155b6bde82ba43cb51cc233c58dcc371d9577
pankajkumar199 <pk228624@gmail.com>
2025-11-19 07:27:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
-//your JS code here. If required.
 const inputs = document.querySelectorAll(".code");
 
 inputs[0].focus();
 
 inputs.forEach((input, index) => {
 
-    input.addEventListener("input", () => {
-        if (input.value.length === 1 && index < inputs.length - 1) {
+    input.addEventListener("input", (e) => {
+        const value = e.target.value;
+
+        if (value.length === 1 && index < inputs.length - 1) {
             inputs[index + 1].focus();
         }
     });
@@ -19,5 +20,4 @@ inputs.forEach((input, index) => {
             }
         }
     });
-
 });
