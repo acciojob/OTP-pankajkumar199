a6457c49b59071b1908376428703c91c110d90fc
pankajkumar199 <pk228624@gmail.com>
2025-11-19 07:31:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,16 +4,16 @@ inputs[0].focus();
 
 inputs.forEach((input, index) => {
 
-    input.addEventListener("input", (e) => {
-        const value = e.target.value;
-
-        if (value.length === 1 && index < inputs.length - 1) {
+    input.addEventListener("input", () => {
+        if (input.value.length === 1 && index < inputs.length - 1) {
             inputs[index + 1].focus();
         }
     });
 
     input.addEventListener("keydown", (e) => {
         if (e.key === "Backspace") {
+
+            // If current is empty → delete previous input
             if (input.value === "" && index > 0) {
                 inputs[index - 1].value = "";
                 inputs[index - 1].focus();
